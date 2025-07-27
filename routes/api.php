98926181430b028669c63e30593adeb6358ba84b
Mochamad Amirul Ikhsani <amirulikhsani41@gmail.com>
2025-07-27 14:20:17 +0700

    add & fix || add categories route

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,22 @@
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', RegisterController::class)->name('register');
+/* Authentication Routes */
+Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
+Route::post('/logout', LogoutController::class);
 
-Route::post('/login', LoginController::class)->name('login');
-
-Route::post('/logout', LogoutController::class)->name('logout');
-
+/* Posts Routes */
 Route::apiResource('/posts', PostsController::class);
+
+/* Categories Routes */
+Route::post('/categories', CategoriesController::class . '@store');
+Route::get('/categories/admin', CategoriesController::class . '@index');
+Route::get('/categories', CategoriesController::class . '@getActiveCategories');
+Route::put('/categories/{id}', CategoriesController::class . '@update');
+Route::delete('/categories/{id}', CategoriesController::class . '@destroy');
